f035b4c27bee52c526aa5f3ca488d254889c758b
Nadia5575 <nadia.sabry2016@feps.edu.eg>
2026-04-17 23:25:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,8 +21,7 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s",
     level=logging.INFO,
     handlers=[
-        logging.StreamHandler(),
-        logging.FileHandler("logs/bot.log", encoding="utf-8")
+        logging.StreamHandler()
     ]
 )
 logger = logging.getLogger(__name__)
